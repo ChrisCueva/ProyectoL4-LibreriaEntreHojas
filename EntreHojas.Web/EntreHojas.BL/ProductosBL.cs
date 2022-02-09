b3af3147d8441d4323c4521c05adbbe074d1229c
ChrisCueva <christian.cueva@unah.hn>
2022-02-08 18:12:08 -0600

    Agregando Entity Framework Code First Avance hasta video 15

--- EntreHojas.Web/EntreHojas.BL/ProductosBL.cs
+++ EntreHojas.Web/EntreHojas.BL/ProductosBL.cs
@@ -9,14 +9,17 @@ namespace EntreHojas.BL
     public class ProductosBL
     {
         Contexto _contexto = new Contexto();
+        public List<Producto> ListadeProductos { get; set; }
 
         public ProductosBL()
         {
             _contexto = new Contexto();
+            ListadeProductos = new List<Producto>();
         }
 
         public List<Producto> ObtenerProductos()
         {
+            ListadeProductos = _contexto.Productos.ToList();
             return _contexto.Productos.ToList();
         }
     }
